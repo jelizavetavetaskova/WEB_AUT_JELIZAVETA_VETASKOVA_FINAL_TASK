689ada977fca2dfb2cc2f7195349296ddd3fb827
Jeļizaveta Vetaškova <s24vetajeli@venta.lv>
2025-03-24 23:37:24 +0200

    Input date of birth

--- cypress/e2e/demoqa/demoqa.cy.js
+++ cypress/e2e/demoqa/demoqa.cy.js
@@ -12,6 +12,11 @@ describe("DEMO QA", () => {
             FormPage.emailField().type("reidjacob@gmail.com");
             FormPage.genderRadio().contains("Male").click();
             FormPage.phonenoField().type("24925708");
+
+            FormPage.dateOfBirthInput().click();
+            FormPage.monthSelect().select("February");
+            FormPage.yearSelect().select("1930");
+            FormPage.dayClick().click();
         });
     });
 })
--- cypress/pageObjects/formPage.js
+++ cypress/pageObjects/formPage.js
@@ -28,4 +28,16 @@ export class FormPage extends BasePage {
     static dateOfBirthInput() {
         return cy.get("#dateOfBirthInput");
     }
+
+    static monthSelect() {
+        return cy.get(".react-datepicker__month-select");
+    }
+
+    static yearSelect() {
+        return cy.get(".react-datepicker__year-select")
+    }
+
+    static dayClick() {
+        return cy.get(".react-datepicker__day--028").not(".react-datepicker__day--outside-month");
+    }
 }
